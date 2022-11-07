c6db1e3285a3718a2f782b25ead76828c024b7fe
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-07 10:11:48 +0000

    edit installed apps sett.py

--- yourcommunity/settings.py
+++ yourcommunity/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-community.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage'
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'blog',
