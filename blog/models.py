df25143ece7b274a86f7297eb1138884509d3874
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-07 13:26:04 +0000

    create class Community Post in models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+# taken from the I think therefore I blog django walkthrough
+
+
+class CommunityPost(models.Model):
+    """ Community Post Model """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    member = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="member_posts"
+    )
+
+    updated_on = models.DateTimeField(auto_now=True)
+    blurb = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(choices=STATUS, default=0)
+    featured_image = CloudinaryField('image', default='placeholder')
+    likes = models.ManyToManyField(
+        User,
+        related_name='recipe_likes',
+        blank=True
+    )
+
+    class Meta:
+        """
+        Dedending order the member posts in created order,
+        """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """
+        Returns a string showing the title.
+        """
+        return str(self.title)
+
+    def number_of_likes(self):
+        """
+        Return total amount of likes on a recipe
+        """
+        return self.likes.count()
